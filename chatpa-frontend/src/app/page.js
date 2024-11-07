5aec2bfb4027f73e892390467c27dd81e17c3b31
mallick-portfolio <tamal1714019@gmail.com>
2024-11-07 22:58:36 +0600

    added frontend next js

---
+++ chatpa-frontend/src/app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h2 className="text-3xl font-bold">Git version control testing</h2>
+	  <p>New line added  df</p>
+    </div>
+  )
+}
+
+export default HomePage
